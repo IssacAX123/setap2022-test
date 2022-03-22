57f7288cba052d256d6edcaffa580b92bd54e747
Issac Abraham <issaca32@gmail.com>
2022-03-22 19:37:15 +0000

    ssss

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 from flask_restful import Api
 from endpoints.task import Task
@@ -7,18 +9,18 @@ app = Flask(__name__)
 api = Api(app)
 
 if not app.debug:
-    logging.basicConfig(filename='logs/logs.log', level=logging.WARNING, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+    logging.basicConfig(filename='logs/logs.log', level=logging.WARNING,
+                        format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
     logging.info('\n')
 
 api.add_resource(Task, "/api/v1/task/<string:user_id>/<string:task_id>", endpoint="get-task",
-                     methods=["GET", "DELETE"],
-                     resource_class_kwargs={"mysql": "mysql", "api": api})
+                 methods=["GET", "DELETE"],
+                 resource_class_kwargs={"mysql": "mysql", "api": api})
 api.add_resource(Task, "/api/v1/task/create", endpoint="create_task", methods=["POST"],
                  resource_class_kwargs={"mysql": "mysql", "api": api})
 
 api.add_resource(Task, "/api/v1/task/<string:user_id>/<string:task_id>", endpoint="update-task", methods=["PATCH"],
                  resource_class_kwargs={"mysql": "mysql", "api": api})
 
-
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
